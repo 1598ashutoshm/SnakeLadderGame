3ea7846fb0b1cc92ceca9b27743f4b824a08b312
1598ashutoshm <ashutoshm1598@gmail.com>
2021-04-28 16:56:35 +0530

    Adding: OptionsForLadderSnake

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,13 +1,34 @@
 
 public class SnakeLadder {
 
+	public static final int ladder=1;
+	public static final int snake=2;
 	public static void main(String[] args) {
 		
-		int player1,position=0;
 		
+		int position=0;
 		int diceroll=(int) Math.floor(Math.random() * 10) % 6;
-		System.out.println("DiceRoll Values:" + diceroll);
+		System.out.println(diceroll);
+		position+=diceroll;
+		int option=(int) Math.floor(Math.random() * 10) % 3;
+		System.out.println(option);
+		if(diceroll==option) {
+			switch (option) {
+			case ladder:
+				position+=diceroll;
+				break;
+			case snake:
+				position-=diceroll;
+				break;
+			default:
+				break;				
+				
+			}
+		}
+		System.out.println("Player Position:" + position);
 		
+		
+	
 	}
 
 }
