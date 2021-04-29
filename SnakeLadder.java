f22c3a0064ff18701b2fea65e30c159ff92459e9
1598ashutoshm <ashutoshm1598@gmail.com>
2021-04-29 17:00:16 +0530

    Adding: Declearing Winner

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,34 +5,65 @@ public class SnakeLadder {
 	public static final int snake=2;
 	public static void main(String[] args) {
 		
+		int position1=0;
+		int position2=0;
 		
-		int position=0;
-		int count=0;
-		while(position<100) {
+		int count1=0; 
+		int count2=0;
+		
+		while(position1<100 && position2<100) {
+			count1++;
 			int diceroll=(int) Math.floor(Math.random() * 10) % 6 + 1;
-			count=count+1;
-		    //System.out.println(diceroll);
-		    position+=diceroll;
 		    int option=(int) Math.floor(Math.random() * 10) % 3;
-		    //System.out.println(option);
-		    if(diceroll==option) {
-		    	switch (option) {
-		    	case ladder:
-				     position+=diceroll;
-				     break;
-			    case snake:
-				     position-=diceroll;
-				     break;
-			    default:
-				     break;					
+		    switch (option) { 	    
+		    case ladder:
+				position1+=diceroll;
+				break;
+			case snake:
+				position1-=diceroll;
+				break;
+			default:
+				break;					
 			}
-		}
-		    if(position>100)
-		    	position-=diceroll;
+		    if(position1==100) {
+		    	break;
+		    } else if(position1<0){
+		    	position1=0;
+		    	
+		    } else if(position1>100) {
+		    	position1-=diceroll;
+		    }
 		    
-		//System.out.println("Player Position:" + position + ',' +  "NoofTimesDIcePlayed" + count);
+		    count2++;
+		    int diceroll1=(int) Math.floor(Math.random() * 10) % 6 + 1;
+		    int option1=(int) Math.floor(Math.random() * 10) % 3;
+		    switch (option1) { 	    
+		    case ladder:
+				position2+=diceroll1;
+				break;
+			case snake:
+				position2-=diceroll1;
+				break;
+			default:
+				break;					
+			}
+		    if(position2==100) {
+		    	break;
+		    } else if(position2>100) {
+		    	position2-=diceroll;
+		    } else if(position2<0) {
+		    	position2=0;
+		    }
 		}
-		System.out.println("Player Position:" + position + ',' +  "NoofTimesDIcePlayed" + count);
+		System.out.println("Player1 Position: " + position1);
+		System.out.println("No of time diceRoll: " + count1);
+		System.out.println("Player2 Position: " + position2);
+		System.out.println("No of time diceroll: " + count2);
 		
+		if(position1==100) {
+			System.out.println("Player1 Won The Game");
+		}else {
+			System.out.println("Player2 Won The Game");
+		}
 	}
 }
